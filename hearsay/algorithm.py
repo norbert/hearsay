76d14a8ce0a17e73db96e033522592e283a4f071
Norbert Crombach <norbert.crombach@primetheory.org>
2014-02-23 17:59:32 +0100

    Vectorize distance calculations and split loop

--- hearsay/algorithm.py
+++ hearsay/algorithm.py
@@ -1,11 +1,14 @@
-__all__ = ['Detect', 'DistToReference', 'Dist', 'ProbClass']
+__all__ = ['DetectStream', 'Detect', 'DistToReference', 'Dist', 'ProbClass']
 
 
 import math
 
 
-def Detect(s_inf, N_obs, R_pos, R_neg, gamma=1, theta=1, D_req=1):
-    """Algorithm 1
+import numpy as np
+
+
+def DetectStream(s_inf, N_obs, R_pos, R_neg, gamma=1, theta=1, D_req=1):
+    """Algorithm 1.1
 
     Perform online binary classification on the infinite stream s_inf using
     sets of positive and negative reference signals R_pos and R_neg.
@@ -32,13 +35,9 @@ def Detect(s_inf, N_obs, R_pos, R_neg, gamma=1, theta=1, D_req=1):
             return
         elif len(s) < N_obs:
             continue
-        PosDists, NegDists = [], []
-        for r in R_pos:
-            PosDists.append(DistToReference(s, r))
-        for r in R_neg:
-            NegDists.append(DistToReference(s, r))
-        R = ProbClass(PosDists, gamma) / ProbClass(NegDists, gamma)
-        if R >= theta:
+        s_ = np.array(s)
+        R = Detect(s_, R_pos, R_neg, gamma, theta)
+        if R:
             ConsecutiveDetections += 1
             if ConsecutiveDetections >= D_req:
                 return i
@@ -46,19 +45,36 @@ def Detect(s_inf, N_obs, R_pos, R_neg, gamma=1, theta=1, D_req=1):
             ConsecutiveDetections = 0
 
 
-def DistToReference(s, r):
+def Detect(s, R_pos, R_neg, gamma=1, theta=1):
+    """Algorithm 1.2
+
+    Perform binary classification on the signal s using sets of positive and
+    negative reference signals R_pos and R_neg.
+    """
+
+    PosDists = DistToReference(s, R_pos)
+    NegDists = DistToReference(s, R_neg)
+    R = ProbClass(PosDists, gamma) / ProbClass(NegDists, gamma)
+    return R >= theta
+
+
+def DistToReference(s, R):
     """Algorithm 2
 
     Compute the minimum distance between s and all pieces of r of the same
     length as s.
     """
 
-    N_obs = len(s)
-    N_ref = len(r)
-    MinDist = float('inf')
+    N_obs = s.shape[0]
+    N_ref = R.shape[1]
+    MinDists = None
     for i in range(N_ref - N_obs + 1):
-        MinDist = min(MinDist, Dist(r[i:(i + N_obs)], s))
-    return MinDist
+        D = Dist(R[:, i:(i + N_obs)], s)
+        if MinDists is not None:
+            MinDists = np.fmin(MinDists, D)
+        else:
+            MinDists = D
+    return MinDists
 
 
 def Dist(s, t):
@@ -66,10 +82,7 @@ def Dist(s, t):
 
     Compute the distance between two signals s and t of the same length.
     """
-
-    D = 0
-    for i in range(len(s)):
-        D += (s[i] - t[i]) ** 2
+    D = np.sum((s - t) ** 2, 1)
     return D
 
 
@@ -80,8 +93,5 @@ def ProbClass(Dists, gamma=1):
     certain class, compute a number proportional to the probability that the
     observation belongs to that class.
     """
-
-    P = 0
-    for i in range(len(Dists)):
-        P += math.exp(-gamma * Dists[i])
+    P = np.sum(np.exp(Dists * -gamma))
     return P
