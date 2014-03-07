1b3ec886e4869c22be77da36621255627c479f11
Norbert Crombach <norbert.crombach@primetheory.org>
2014-03-07 18:13:13 +0100

    Add license information

--- setup.py
+++ setup.py
@@ -3,5 +3,10 @@ from setuptools import setup
 
 setup(name='hearsay',
       version='0.0.1',
+      description='Models for non-parametric time series classification.',
+      author='Norbert Crombach',
+      author_email='norbert.crombach@primetheory.org',
+      url='https://github.com/norbert/hearsay',
+      license='MIT',
       packages=['hearsay'],
       install_requires=['scikit-learn'])
